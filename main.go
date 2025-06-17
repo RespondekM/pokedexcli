fe240acda3a2051d89534667d94495ed304d1951
RespondekM <respondekm@mailbox.org>
2025-06-17 23:54:46 +0200

    Second commit

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import ("fmt")
+import ("fmt"
+	"strings")
 
 func main() {
 fmt.Println("Hello, World!")
+}
+
+func cleanInput(text string) []string {
+	result := strings.Split(strings.Trim(strings.ToLower(text)," ")," ")
+	return result
 }
